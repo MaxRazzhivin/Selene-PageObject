a7958e992adc14f3e8ac0d95cb13964cf06dec3b
Max_Razzhivin <max.nvo06@gmail.com>
2025-07-18 22:37:07 +0300

    fix mistake in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,7 +32,7 @@ def browser_management():
         # Указание пути к ChromeDriver вручную
         service = Service(executable_path='/usr/local/bin/chromedriver')
         browser.config.driver = webdriver.Chrome(service=service,
-                                                 options=options)
+                                                 options=driver_options)
 
     browser.config.driver_options = driver_options
 
